367d5abb1d7db76d081a6d4267b4ef2b5c62dd8b
skipkayhil <skipkayhil@gmail.com>
2018-09-25 17:13:20 -0400

    add prettier formatting

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 // TODO:
-// - Ensure game looks nice in landscape mode -> move to Grid instead of flex?
+// - Ensure game looks nice in landscape mode -> move to Grid instead of flex
+// - Change checkwin to only check tiles in last move's row/col/(diag)
 // - Refactor getHardAITurn to use getStateChildren better
 //    - additional optimizations to getStateChildren
 //      - add tests so that the debug information isn't needed
@@ -12,7 +13,7 @@
 //    - either way every tile needs to be iterated and enabled so it doesn't matter?
 'use strict';
 
-function getStateChildren (tilesLeft, playerTiles, opponentTiles, tileID) {
+function getStateChildren(tilesLeft, playerTiles, opponentTiles, tileID) {
   const retValue = [0, null, tileID]; // 0: best move, 1: score, 2: tileID
   const depth = playerTiles.length + opponentTiles.length;
   const player = depth % 2 === 1 ? 'x' : 'o';
@@ -33,12 +34,14 @@ function getStateChildren (tilesLeft, playerTiles, opponentTiles, tileID) {
       );
       //add a '[]' to retValue initialization to use this
       //retValue[3] = [...retValue[3], child]; //[3] is the child array
-      if (possible.length === 0 ||
+      if (
+        possible.length === 0 ||
         (player === 'x' && child[1] < possible[0][1]) ||
-        (player === 'o' && child[1] > possible[0][1])) {
-          // if this is the first child, or the score is better,
-          // then add the child to possible children
-          possible = [child];
+        (player === 'o' && child[1] > possible[0][1])
+      ) {
+        // if this is the first child, or the score is better,
+        // then add the child to possible children
+        possible = [child];
       } else if (child[1] == possible[0][1]) {
         possible = [...possible, child];
       }
@@ -54,7 +57,7 @@ window.addEventListener('load', () => {
   setupGame('off');
 });
 
-function setupGame (aiSetting) {
+function setupGame(aiSetting) {
   const defaultState = {
     x: [],
     o: [],
@@ -64,7 +67,7 @@ function setupGame (aiSetting) {
   };
   const tileClicked = setupGameState(defaultState, aiSetting);
 
-  getElementArray('tile').forEach((tile) => {
+  getElementArray('tile').forEach(tile => {
     tile.onclick = () => tileClicked(parseInt(tile.id));
     tile.className = 'tile empty enabled';
 
@@ -74,14 +77,14 @@ function setupGame (aiSetting) {
   if (aiSetting === 'x') doAITurn(tileClicked, defaultState);
 }
 
-function getOtherPlayer (currentTurn) {
+function getOtherPlayer(currentTurn) {
   return currentTurn === 'x' ? 'o' : 'x';
 }
 
-function setupGameState (defaultState, aiSetting) {
+function setupGameState(defaultState, aiSetting) {
   let state = defaultState;
 
-  return function tileClicked (tileID) {
+  return function tileClicked(tileID) {
     const tile = document.getElementById(tileID);
     const player = state.nextPlayer;
     const check = checkWin(state[player], [tileID]);
@@ -101,7 +104,7 @@ function setupGameState (defaultState, aiSetting) {
     if (check) {
       console.log(player + ' has won!');
 
-      check.forEach((tile) => {
+      check.forEach(tile => {
         document.getElementById(tile).className += ' win';
       });
 
@@ -111,22 +114,22 @@ function setupGameState (defaultState, aiSetting) {
     } else if (state.nextPlayer === state.ai) {
       doAITurn(tileClicked, state);
     }
-  }
+  };
 }
 
-function doAITurn (tileClicked, state, currentAIMode) {
+function doAITurn(tileClicked, state, currentAIMode) {
   tileClicked(getHardAITurn(state));
 }
 
-function getEasyAITurn ({x, o}) {
+function getEasyAITurn({ x, o }) {
   const rand = Math.floor(Math.random() * 9) + 1;
 
   return !x.includes(rand) && !o.includes(rand)
     ? rand
-    : getEasyAITurn({x, o});
+    : getEasyAITurn({ x, o });
 }
 
-function getMedAITurn ({ai, ...state}) {
+function getMedAITurn({ ai, ...state }) {
   // try to Win
   const winningMove = findThirdTile(state[ai]);
   if (winningMove != null) return winningMove;
@@ -139,13 +142,16 @@ function getMedAITurn ({ai, ...state}) {
   console.log('this turn is random');
   return getEasyAITurn(state);
 
-  function findThirdTile (squareArr) {
+  function findThirdTile(squareArr) {
     if (squareArr.length < 2) return null;
     for (var i = 0; i < squareArr.length - 1; i++) {
       for (var j = i + 1; j < squareArr.length; j++) {
         const id = 15 - squareArr[i] - squareArr[j];
-        if (id > 0 && id < 10 &&
-          document.getElementById(id).className.includes('empty')) {
+        if (
+          id > 0 &&
+          id < 10 &&
+          document.getElementById(id).className.includes('empty')
+        ) {
           return id;
         }
       }
@@ -153,7 +159,7 @@ function getMedAITurn ({ai, ...state}) {
   }
 }
 
-function getHardAITurn ({turns, ai, ...state}) {
+function getHardAITurn({ turns, ai, ...state }) {
   // Skynet came online 2016/11/19 at 19:43:07
   if (turns === 0) {
     return pickRandomCorner(); // if every tile is empty, pick a random corner
@@ -174,19 +180,19 @@ function getHardAITurn ({turns, ai, ...state}) {
     return turn[0];
   }
 
-  function pickRandomCorner () {
+  function pickRandomCorner() {
     return (Math.floor(Math.random() * 4) + 1) * 2;
   }
 }
 
-function getElementArray (className) {
+function getElementArray(className) {
   // This is literally just a wrapper to allow forEach usage.
   // getElementsByClassName returns a NodeList, which doesn't universally
   // support forEach yet.
   return Array.from(document.getElementsByClassName(className));
 }
 
-function checkWin (numbers, partial) {
+function checkWin(numbers, partial) {
   //  Goal: return array of winning tile values
   //  if there are no winning values, return false
   var sum;
@@ -207,26 +213,29 @@ function checkWin (numbers, partial) {
     const remaining = numbers.slice(i + 1);
     const check = checkWin(remaining, partial.concat([n]));
     if (check) {
-      check.forEach((winningTile) => {
+      check.forEach(winningTile => {
         if (winningTiles.indexOf(winningTile) === -1) {
           winningTiles = winningTiles.concat(winningTile);
         }
       });
     }
   }
-  return winningTiles.length === 0
-    ? false
-    : winningTiles;
+  return winningTiles.length === 0 ? false : winningTiles;
 }
 
 // Possibly deprecate this, it's only used once
-function gameOver (aiSetting) {
-  getElementArray('tile').forEach((tile) => {
+function gameOver(aiSetting) {
+  getElementArray('tile').forEach(tile => {
     if (tile.id === '5') {
-      tile.onclick = () => { setupGame(aiSetting); };
+      tile.onclick = () => {
+        setupGame(aiSetting);
+      };
 
-      tile.appendChild(document.createElement('DIV').appendChild(
-        document.createTextNode('NEW GAME')));
+      tile.appendChild(
+        document
+          .createElement('DIV')
+          .appendChild(document.createTextNode('NEW GAME'))
+      );
 
       if (!tile.className.includes('enabled')) tile.className += ' enabled';
     } else {
@@ -237,14 +246,14 @@ function gameOver (aiSetting) {
   });
 }
 
-function toggleSettingsMenu () {
+function toggleSettingsMenu() {
   document.getElementById('settings').className =
     document.getElementById('settings').className === 'hidden'
       ? 'visible'
       : 'hidden';
 }
 
-function settingClicked (aiSetting) {
+function settingClicked(aiSetting) {
   console.log('AI is ' + aiSetting);
   toggleSettingsMenu();
   setupGame(aiSetting);
